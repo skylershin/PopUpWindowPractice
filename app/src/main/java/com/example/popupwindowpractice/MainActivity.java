49fa81437bd56c37d7caa4d229366a06a825d24a
MunkyuShin <munkyu.s.shin@gmail.com>
2017-01-24 19:56:11 +0900

    remove background code

--- app/src/main/java/com/example/popupwindowpractice/MainActivity.java
+++ app/src/main/java/com/example/popupwindowpractice/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.popupwindowpractice;
 
-import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.PopupWindowCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
@@ -28,9 +25,6 @@ public class MainActivity extends AppCompatActivity {
                 PopupWindow popupWindow = new PopupWindow(popUpView,
                         RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
                 popupWindow.showAtLocation(popUpView, Gravity.CENTER, 0, 0);
-
-                popupWindow.setBackgroundDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.background_gradient));
-
             }
         });
     }
